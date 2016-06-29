8aa0d1b6892bfa048d90d39719b0dc3172fa8b37
Omar Kassouar <omar@varagesale.com>
2016-06-29 00:58:05 -0400

    set up views and routes for basic TODO tracker

---
+++ README.md
@@ -0,0 +1,7 @@
+## Mini List App
+
+This repo is just a test to play around with Sinatra and practice stuffs. It was
+thrown together in a few minutes and then prompty forgotten about.
+
+Please don't try to actually use anything here, it's most likely awful. You've
+been warned!
---
+++ to_do_tracker.rb
@@ -0,0 +1,69 @@
+require 'sinatra'
+require 'data_mapper'
+require 'tilt/erb'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/to_do_tracker.db")
+
+class Note
+  include DataMapper::Resource
+  property :id, Serial 
+  property :content, Text, :required => true
+  property :complete, Boolean, :required => true, :default => false
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
+
+get '/' do
+  @notes = Note.all :order => :id.desc
+  @title = 'All Notes'
+  erb :home
+end
+
+post '/' do
+  n = Note.new
+  n.content = params[:content]
+  n.created_at = Time.now
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+get '/:id' do
+  @note = Note.get params[:id]
+  @title = "Edit note ##{params[:id]}"
+  erb :edit
+end
+
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0 
+  # complete param only exists if checkbox was checked when form was submitted
+  # therefore set bool depending on whether it exists
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+get '/:id/delete' do
+  @note = Note.get params[:id]
+  @title = "Confirm deletion of note ##{params[:id]}"
+  erb :delete
+end
+
+delete '/:id' do
+  n = Note.get params[:id]
+  n.destroy
+  redirect '/'
+end
+
+get '/:id/complete' do
+  n = Note.get params[:id]
+  # reverse complete status
+  n.complete = n.complete ? 0 : 1
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
