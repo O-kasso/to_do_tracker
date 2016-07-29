f0a5e65ed07cd269afe44faf6966b14ea661272a
Omar Kassouar <omar@varagesale.com>
2016-07-29 01:29:10 -0400

    add notice and error flashes to all pages

--- to_do_tracker.rb
+++ to_do_tracker.rb
@@ -31,44 +31,68 @@ post '/' do
   n.content = params[:content]
   n.created_at = Time.now
   n.updated_at = Time.now
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', notice: 'Note created successfully.'
+  else
+    redirect '/', notice: 'Failed to save note.'
+  end
 end
 
 get '/:id' do
   @note = Note.get params[:id]
   @title = "Edit note ##{params[:id]}"
-  erb :edit
+  if @note
+    erb :edit
+  else
+    redirect '/', error: 'Cannot find note.'
+  end
 end
 
 put '/:id' do
   n = Note.get params[:id]
+  unless n
+    redirect '/', error: 'Cannot find note.'
+  end
   n.content = params[:content]
   n.complete = params[:complete] ? 1 : 0 
   # complete param only exists if checkbox was checked when form was submitted
   # therefore set bool depending on whether it exists
   n.updated_at = Time.now
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', notice: 'Note updated successfully.'
+  else
+    redirect '/', error: 'Error updating note.'
+  end
 end
 
 get '/:id/delete' do
   @note = Note.get params[:id]
   @title = "Confirm deletion of note ##{params[:id]}"
-  erb :delete
+  if @note
+    erb :delete
+  else
+    redirect '/', error: 'Cannot find note.'
+  end
 end
 
 delete '/:id' do
   n = Note.get params[:id]
-  n.destroy
-  redirect '/'
+  if n.destroy
+    redirect '/', notice: 'Note deleted successfully.'
+  else
+    redirect '/', error: 'Error deleting note.'
+  end
 end
 
 get '/:id/complete' do
   n = Note.get params[:id]
+  redirect '/', error: 'Cannot find note.' unless n
   # reverse complete status
   n.complete = n.complete ? 0 : 1
   n.updated_at = Time.now
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', notice: 'Note marked as complete.'
+  else
+    redirect '/', error: 'Error marking note as complete.'
+  end
 end
